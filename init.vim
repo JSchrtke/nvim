58f2bca736f06d3149a24e12a0dc1dc8c4e45dd9
JSchrtke <jschrtke@protonmail.com>
2022-09-06 08:12:50 +0200

    Add package manager

--- init.vim
+++
@@ -1,6 +0,0 @@
-" ### Install plugins ###
-lua << EOF
-require("packer").startup(function()
-    use "wbthomason/packer.nvim"
-end)
-EOF
---
+++ lua/plugins.lua
@@ -0,0 +1,5 @@
+require("packer").startup(function()
+use "wbthomason/packer.nvim"
+use "nvim-telescope/telescope.nvim"
+use "nvim-lua/plenary.nvim"
+end)
