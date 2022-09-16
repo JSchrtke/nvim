2ccb8f1eb27b8d31884751142c05e5e2e60a8d4f
JSchrtke <jschrtke@protonmail.com>
2022-09-16 15:00:30 +0200

    Use 'smart send to qflist' for telescope

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,3 +1,4 @@
+local actions = require('telescope.actions')
 local layout = require('telescope.actions.layout')
 require("telescope").setup({
     defaults = {
@@ -10,9 +11,11 @@ require("telescope").setup({
         mappings = {
             i = {
                 ["<M-p>"] = layout.toggle_preview,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
             },
             n = {
                 ["<M-p>"] = layout.toggle_preview,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
             },
         },
     }
