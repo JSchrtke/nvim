6f8eff6a35c455a25e12933abaaccb1fa693c7b8
jschrtke <jschrtke@protonmail.com>
2022-10-09 19:31:49 +0200

    simplify mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,41 +2,28 @@ vim.g.mapleader = ' '
 
 local map = vim.api.nvim_set_keymap
 
--- movements
-map('n', '<leader>h', '<C-w>h', {})
-map('n', '<leader>j', '<C-w>j', {})
-map('n', '<leader>k', '<C-w>k', {})
-map('n', '<leader>l', '<C-w>l', {})
-
--- window management
-map('n', '<leader>wo', '<C-w>o', {})
-map('n', '<leader>wq', '<C-w>q', {})
-map('n', '<leader>wh', '<C-w>v', {})
-map('n', '<leader>wj', '<C-w>s<C-w>j', {})
-map('n', '<leader>wk', '<C-w>s<C-w>k', {})
-map('n', '<leader>wl', '<C-w>v<C-w>l', {})
-map('n', '<leader>H', '<C-w>H', {})
-map('n', '<leader>J', '<C-w>J', {})
-map('n', '<leader>K', '<C-w>K', {})
-map('n', '<leader>L', '<C-w>L', {})
+-- quickfix list
+map('n', '<leader>n', ':cnext<cr>', {})
+map('n', '<leader>p', ':cprev<cr>', {})
 
 -- telescope
-map('n', '<leader>of', ':Telescope find_files<cr>', {})
-map('n', '<leader>ob', ':Telescope buffers<cr>', {})
-map('n', '<leader>ff', ':Telescope current_buffer_fuzzy_find<cr>', {})
-map('n', '<leader>fd', ':Telescope live_grep<cr>', {})
-map('n', '<leader>fw', ':Telescope grep_string<cr>', {})
-map('n', '<leader>fh', ':Telescope help_tags<cr>', {})
-map('n', '<leader>fo', ':Telescope oldfiles<cr>', {})
+map('n', '<leader>o', ':Telescope find_files<cr>', {})
+map('n', '<leader>b', ':Telescope buffers<cr>', {})
+map('n', '<leader>/', ':Telescope current_buffer_fuzzy_find<cr>', {})
+map('n', '<leader>g', ':Telescope live_grep<cr>', {})
+map('n', '<leader>w', ':Telescope grep_string<cr>', {})
+map('n', '<leader>?', ':Telescope help_tags<cr>', {})
+map('n', '<leader>O', ':Telescope oldfiles<cr>', {})
 map('n', '<leader><leader>', ':Telescope resume<cr>', {})
+
 
 -- harpoon
 map('n', '<leader>m', ':lua require("harpoon.mark").add_file()<cr>', {})
-map('n', '<leader>sh', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', {})
-map('n', 'gh', ':lua require("harpoon.ui").nav_file(1)<cr>', {})
-map('n', 'gj', ':lua require("harpoon.ui").nav_file(2)<cr>', {})
-map('n', 'gk', ':lua require("harpoon.ui").nav_file(3)<cr>', {})
-map('n', 'gl', ':lua require("harpoon.ui").nav_file(4)<cr>', {})
+map('n', '<leader>M', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', {})
+map('n', '<leader>h', ':lua require("harpoon.ui").nav_file(1)<cr>', {})
+map('n', '<leader>j', ':lua require("harpoon.ui").nav_file(2)<cr>', {})
+map('n', '<leader>k', ':lua require("harpoon.ui").nav_file(3)<cr>', {})
+map('n', '<leader>l', ':lua require("harpoon.ui").nav_file(4)<cr>', {})
 map('n', '<leader>t', ':lua require("harpoon.term").gotoTerminal(1)<cr>', {})
 
 -- terminal mappings
@@ -57,9 +44,14 @@ map("n", "<c-n>", "<Plug>(YankyCycleForward)", {})
 map("n", "<c-p>", "<Plug>(YankyCycleBackward)", {})
 
 -- gitsigns
-map("n", "<leader>nc", ":Gitsigns next_hunk<cr>", {})
-map("n", "<leader>pc", ":Gitsigns prev_hunk<cr>", {})
 map("n", "<leader>sc", ":Gitsigns preview_hunk<cr>", {})
+map("n", "<leader>c", ":Gitsigns next_hunk<cr>", {})
+map("n", "<leader>C", ":Gitsigns prev_hunk<cr>", {})
 
 -- spectre
 map("n", "<leader>R", ":lua require('spectre').open_visual({select_word=true})<CR>", {})
+
+-- diagnostic
+map("n", "<leader>sd", ":lua vim.diagnostic.open_float()<CR>", {})
+map("n", "<leader>d", ":lua vim.diagnostic.goto_next()<CR>", {})
+map("n", "<leader>D", ":lua vim.diagnostic.goto_prev()<CR>", {})
