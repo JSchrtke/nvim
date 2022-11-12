28731981782fe85c08f9981258b9043282f8cfeb
jschrtke <jschrtke@protonmail.com>
2022-11-12 18:32:53 +0100

    Add custom toggle function for lsp-lines

--- lua/lsp-lines-config.lua
+++ lua/lsp-lines-config.lua
@@ -1,4 +1,20 @@
-require("lsp_lines").setup()
+M = {}
+
+local lsp_lines = require("lsp_lines")
+
+lsp_lines.setup()
 vim.diagnostic.config({
     virtual_text = false,
 })
+
+function M.toggle()
+    lsp_lines.toggle()
+    local old_value = vim.diagnostic.config().virtual_text
+    vim.diagnostic.config({
+        virtual_text = not old_value
+    })
+end
+
+M.toggle()
+
+return M
