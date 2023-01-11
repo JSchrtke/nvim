3a323bc090163d38fe741a1da465751c98fe19fd
JSchrtke <jschrtke@protonmail.com>
2023-01-11 16:53:43 +0100

    Remove 'treesitter-context' plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,13 +7,13 @@ use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
 }
+use 'nvim-treesitter/playground'
 use 'JSchrtke/harpoon'
 use 'rkennedy/vim-delphi'
 use 'gbprod/yanky.nvim'
 use 'ishan9299/nvim-solarized-lua'
 use 'tpope/vim-commentary'
 use 'lewis6991/gitsigns.nvim'
-use 'nvim-treesitter/nvim-treesitter-context'
 use 'ethanholz/nvim-lastplace'
 use 'ishan9299/modus-theme-vim'
 use 'Pocco81/auto-save.nvim'
--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -4,4 +4,7 @@ require'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
     },
+    playground = {
+        enable = true,
+    },
 }
