56c0885adb54984913aed79b725f24f602bc0180
JSchrtke <jschrtke@protonmail.com>
2023-03-01 12:45:50 +0100

    Adjust colorcolumn to 100

--- lua/settings.lua
+++ lua/settings.lua
@@ -15,6 +15,7 @@ vim.o.wrap = true
 vim.o.listchars="tab:-->,eol:↲,nbsp:␣,space:•,trail:•,extends:⟩,precedes:⟨"
 vim.o.cursorline = true
 vim.o.laststatus = 3
+vim.o.colorcolumn = "100"
 
 function Winbar()
     local navic = require("nvim-navic")
