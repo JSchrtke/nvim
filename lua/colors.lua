69ff9caf5d7ac39b3f2ed821d6b9fa5c340ea6b1
Johannes <j.schroetke@wertgarantie.com>
2023-05-05 18:17:30 +0200

    Adjust color scheme

--- lua/colors.lua
+++ lua/colors.lua
@@ -19,7 +19,7 @@
 
 vim.g.modus_dim_inactive_window=false
 vim.cmd("colorscheme modus-vivendi")
-vim.cmd("highlight! link WinBar NormalFloat")
+vim.cmd("highlight WinBar gui=bold guibg=#1e1e1e")
 vim.cmd("highlight! DiffText gui=bold guifg=#2a2000 guibg=#b0ba9f")
 
 -- require('github-theme').setup({
