e3920ad0deb2a4f36123a1aa52adcc6fa3017f42
jschrtke <jschrtke@proton.me>
2023-07-18 15:16:33 +0200

    keymaps: add mapping to build project

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -41,6 +41,7 @@ map('n', '<leader>rk', "<cmd>lua vim.ui.input({ prompt = '3 > '}, function(input
 -- run things
 map('n', '<leader>rp', "<cmd>lua require('harpoon.term').sendCommand(1, './run.sh')<cr><cmd>lua require('harpoon.term').gotoTerminal(1)<cr>", {})
 map('n', '<leader>rt', "<cmd>lua require('harpoon.term').sendCommand(1, './test.sh')<cr><cmd>lua require('harpoon.term').gotoTerminal(1)<cr>", {})
+map('n', '<leader>rb', "<cmd>lua require('harpoon.term').sendCommand(1, './build.sh')<cr><cmd>lua require('harpoon.term').gotoTerminal(1)<cr>", {})
 map('n', '<leader>rf', ":lua vim.lsp.buf.format()<cr>", {})
 
 -- terminal mappings
